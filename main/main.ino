561508ad43beb9eda61f779e652d134a82297451
Jonatan <jonatan99999@googlemail.com>
2017-04-02 15:09:20 +0200

    Addition of Wall Auto-Stop
    Robot now stops automatically when hitting a wall while driving forward or backward.
    Also: Code Cleanup, introduction of Constants and Commenting

--- main/main.ino
+++ main/main.ino
@@ -2,69 +2,110 @@
  * Müllabfuhr
  */
 
-//#include <Zumo32U4Encoders.h>
+//Import
+#include <Wire.h>
 #include <Zumo32U4Motors.h>
 #include <Zumo32U4LCD.h>
 #include <Zumo32U4.h>
+//#include <Zumo32U4Encoders.h>
 //#include <Zumo32U4LineSensors.h>
 
+//Objekte
+LSM303 compass;
 Zumo32U4Motors motors;
 Zumo32U4LCD lcd;
 Zumo32U4ButtonA a;
 Zumo32U4ButtonB b;
 Zumo32U4ButtonC c;
 
-int dSpeed = 200; // Driving Speed
-int tSpeed = 100; // Turning Speed
+//Konstanten
+const int SLOW = 1;
+const int FAST = 2;
+const int FORWARD = 1;
+const int BACKWARD = -1;
+const int baseSpeed = 100;
+const int rCorrection = -5;
+const int lCorrection = 0;
+const int turnDelay = 600;
+
+//Variablen
+int force;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Initialisierung des Beschleunigungssensors
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+
+  //Bildschirmausgabe, warten auf Startsignal
+  lcd.print(readBatteryMillivolts());
   a.waitForButton();
+
+  //Hauptprogramm
+  lcd.clear();
   lcd.print("Fahren...");
-  forward(2000);
-  back(3000);
-  forward(400,0);
+  drive(FORWARD,FAST,2000);
+  drive(BACKWARD,FAST,3000);
+  drive(FORWARD,SLOW,400);
   turnLeft();
-  forward(400,0);
+  drive(FORWARD,SLOW,400);
   turnRight();
 
-  forward(3000);
-  back(3200);
-  forward(400,0);
+  drive(FORWARD,FAST,3000);
+  drive(BACKWARD,FAST,3200);
+  drive(FORWARD,SLOW,400);
   turnLeft();
-  forward(600,0);
+  drive(FORWARD,SLOW,600);
   turnRight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
 
-void forward(int t) {
-  motors.setSpeeds(dSpeed,dSpeed-5);
-  delay(t);
+/*
+ * Lässt den Roboter fahren
+ * 
+ * @params direction  Richtung
+ * @params sMod       Geschwindigkeitsmodifikator
+ * @params duration   maximale Fahrzeit
+ */
+void drive(int direction, int sMod, int duration) {
+  int endTime = millis() + duration;                            //Berechnet Zeitpunkt an dem die Methode spätestens beendet wird
+  int lSpeed = direction * ((sMod * baseSpeed) + lCorrection);  //Berechnet Geschwindigkeit der linken Seite
+  int rSpeed = direction * ((sMod * baseSpeed) + rCorrection);  //Berechnet Geschwindigkeit der rechten Seite
+  motors.setSpeeds(lSpeed, rSpeed);
+  
+  if (duration > 900) {   //Wenn Fahrt über 900 ms -> Wanderkennung
+    delay(500);           //Warten bis konstante Geschwindigkeit erreicht wurde
+    do {
+      compass.readAcc();  //Kompass auslesen
+      //Werte aller Achsen addieren und 1 G subtrahieren
+      force = (abs(compass.a.x) + abs(compass.a.y) + abs(compass.a.z)) - 16500;
+      delay(10);
+      lcd.clear();
+      lcd.print(force);
+    } while (force < 15000 && millis() < endTime); //Abbruch wenn Kraftänderung über 15000 (fast 1 G) oder wenn Endzeit erreicht
+  } else {
+    delay(duration);
+  }
 }
 
-void forward(int t, int p) {
-  motors.setSpeeds(tSpeed,tSpeed);
-  delay(t);
-}
-
-void back(int t) {
-  motors.setSpeeds(-dSpeed,-dSpeed);
-  delay(t);
-}
-
+/*
+ * Dreht den Roboter um 90 Grad nach links
+ */
 void turnRight() {
-  motors.setSpeeds(100,-100);
-  delay(600);
+  motors.setSpeeds(baseSpeed,-baseSpeed);
+  delay(turnDelay);
   motors.setSpeeds(0,0);
 }
 
+/*
+ * Dreht den Roboter um 90 Grad nach rechts
+ */
 void turnLeft() {
-  motors.setSpeeds(-100,100);
-  delay(600);
+  motors.setSpeeds(-baseSpeed,baseSpeed);
+  delay(turnDelay);
   motors.setSpeeds(0,0);
 }
 
